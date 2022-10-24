b1bd62503c4eac63a93d01c4f0191f568d15ac58
Paul Thomas <paulthomas2107@gmail.com>
2022-10-24 16:39:31 +0100

    More....

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
-const url = require('url')
+const url = require('url');
 const express = require('express');
 const router = express.Router();
 const needle = require('needle');
@@ -11,19 +11,19 @@ const API_KEY_VALUE = process.env.API_KEY_VALUE;
 
 router.get('/', async (req, res) => {
   try {
-
-
-
     const params = new URLSearchParams({
-        
-        [API_KEY_NAME]: API_KEY_VALUE,
-        ...url.parse(req.url, true).query,
-    })
-
+      [API_KEY_NAME]: API_KEY_VALUE,
+      ...url.parse(req.url, true).query,
+    });
 
     const apiRes = await needle('get', `${API_BASE_URL}?${params}`);
-
     const data = apiRes.body;
+
+    // Log request to public API
+    if (process.env.NODE_ENV != 'production') {
+        console.log(`REQUEST: ${API_BASE_URL}?${params}`)
+    }
+
     res.status(200).json(data);
   } catch (error) {
     res.status(500).json(error);
